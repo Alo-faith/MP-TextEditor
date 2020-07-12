61988a737da1c21dc5a22f24256f1f5710bed9e1
alo-faith <alo.faith@gmail.com>
2020-07-12 16:01:31 +0300

    style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const styles = {
@@ -8,12 +8,29 @@ const styles = {
 };
 
 const stylings = ["bold", "italic", "underline"];
-
 const colors = ["yellow", "blue", "red", "black", "purple"];
 
 function App() {
+  const [editColor, setColor] = useState("black");
+  let [editStyle, setStyle] = useState("normal");
+  let [styling, setStyling] = useState({});
+  // let [x, setx] = useState(Object.values(styles));
+
+  const newStyle = (style) => {
+    // setStyling(styles[style]);
+
+    console.log(x);
+  };
+  const x = Object.values(styles);
   const stylingBoxes = stylings.map((style) => (
-    <button className="btn btn-light" style={styles[style]} key={style}>
+    <button
+      className="btn btn-light"
+      style={styles[style]}
+      key={style}
+      onClick={() => {
+        setStyle(style);
+      }}
+    >
       {style}
     </button>
   ));
@@ -22,13 +39,22 @@ function App() {
     <button
       style={{ backgroundColor: color, height: 30, width: 30 }}
       key={color}
+      onClick={() => {
+        setColor(color);
+      }}
     />
   ));
 
   return (
     <div className="App">
-      <div className="my-3">{stylingBoxes}</div>
-      <textarea />
+      <div className="my-3">{stylingBoxes} </div>
+
+      {/* <textarea
+        style={{ color: editColor, ...styles["bold"], ...styles["italic"] }}
+      /> */}
+
+      <textarea style={{ color: editColor, ...styles[editStyle] }} />
+
       <div className="my-3">{colorBoxes}</div>
     </div>
   );
